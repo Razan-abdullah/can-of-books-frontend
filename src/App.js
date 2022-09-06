2ac383ff6ec5e629086a369da701f3ef77cb7d60
Razan-abdullah <razanaalabdallah017@gmail.com>
2022-09-06 04:35:19 +0300

    d

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
 import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import BestBooks from './BestBooks';
 import axios from 'axios';
-import Form from './form';
 
 class App extends React.Component {
+constructor(props){
+  super(props);
+  this.state = {
+    bookArr : []
+  }
+}
 
+  componentDidMount = () => {
+    axios
+    .get(`http://localhost:3100/getbooks`)
+    .then(result =>{
+      console.log(result.data);
+      this.setState({
+        bookArr : result.data
+      })
+    })
+    .catch(err=>{
+      console.log(err);
+    })
+    
+  }
 
+  render(){
+    return(
+      <div>
+        <h1>Book  System</h1>
+        {this.state.bookArr.map(item =>{
+          return(
+            <div>
+              <h3> title : {item.title} </h3>
+              <p>description  : {item.des}</p>
+              <p>status  : {item.status}</p>
 
-
-
- 
-  render() {
-    return (
-<div>
-        
-
-<Header/>
-<BestBooks/>
-
-<Footer/></div>
-
-
-
+            </div>
+          )
+        })}
+      </div>
     )
   }
 }
